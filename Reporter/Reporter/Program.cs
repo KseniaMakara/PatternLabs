6c5af03e35d7aba7a415df3d38fc1f9feca7b9a6
KseniaMakara <makara.kseniya@student.uzhnu.edu.ua>
2022-10-05 15:34:49 +0300

    builder

---
+++ Reporter/Reporter/Program.cs
@@ -0,0 +1,21 @@
+﻿using Reporter;
+
+List<Employee> employees = new()
+{
+    new Employee { Name = "Langa", Salary = 100 },
+    new Employee { Name = "Levi", Salary = 50 },
+    new Employee { Name = "Kaneki", Salary = 200 },
+    new Employee { Name = "Hisoka", Salary = 100 }
+};
+
+var builder = new EmployeeReportBuilder(employees);
+
+var director = new EmployeeReportDirector(builder);
+
+director.Build();
+
+var report = builder.GetReport();
+
+Console.WriteLine(report);
+
+Console.ReadKey();
