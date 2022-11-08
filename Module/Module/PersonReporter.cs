532a640976eb2682770e46d75612aaf257cd7208
KseniaMakara <makara.kseniya@student.uzhnu.edu.ua>
2022-11-08 10:42:36 +0200

    module

---
+++ Module/Module/PersonReporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    public class Person
+    {
+        public string Name;
+        public string Photo;
+        public string Education;
+        public string Exp;
+        public int Age;
+        public DateTime BirthDate;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+            clone.Education = String.Copy(Education);
+            return clone;
+        }
+    }
+}
