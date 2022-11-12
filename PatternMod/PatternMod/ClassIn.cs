a8b8b8be43a2971e0fb440c5ce3c9b1ffe9d81b9
KseniaMakara <makara.kseniya@student.uzhnu.edu.ua>
2022-11-12 16:06:56 +0200

    moduleBuilder

---
+++ PatternMod/PatternMod/ClassIn.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace PatternMod
+{
+    class Name
+    {
+        public string Sort { get; set; }
+    }
+    class Photo
+    {
+        public string Sort { get; set; }
+    }
+    class University
+    {
+        public string Sort { get; set; }
+    }
+    class Exp
+    {
+        public string Sort { get; set; }
+    }
+
+    class Applicant
+    {
+        public Name Name { get; set; }
+        public Photo Photo { get; set; }
+        public University University { get; set; }
+        public Exp Exp { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Name != null)
+                sb.Append(Name.Sort + "\n");
+            if (Photo != null)
+                sb.Append(Photo.Sort + "\n");
+            if (University != null)
+                sb.Append(University.Sort + " \n");
+            if (Exp != null)
+                sb.Append(Exp.Sort + " \n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ PatternMod/PatternMod/Program.cs
@@ -0,0 +1,85 @@
+﻿using PatternMod;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Maker maker = new Maker();
+
+        AppBuilder builder = new Junior();
+        Applicant jun = maker.Make(builder);
+        Console.WriteLine(jun.ToString());
+
+        builder = new Senior();
+        Applicant sen = maker.Make(builder);
+        Console.WriteLine(sen.ToString());
+
+        Console.Read();
+    }
+}
+abstract class AppBuilder
+{
+    public Applicant Applicant { get; private set; }
+    public void CreateApp()
+    {
+        Applicant = new Applicant();
+    }
+    public abstract void SetName();
+    public abstract void SetPhoto();
+    public abstract void SetUniversity();
+    public abstract void SetExp();
+}
+class Maker
+{
+    public Applicant Make(AppBuilder appBuilder)
+    {
+        appBuilder.CreateApp();
+        appBuilder.SetName();
+        appBuilder.SetPhoto();
+        appBuilder.SetUniversity();
+        appBuilder.SetExp();
+        return appBuilder.Applicant;
+    }
+}
+class Junior : AppBuilder
+{
+    public override void SetName()
+    {
+        this.Applicant.Name = new Name { Sort = "Killua Zoldyck" };
+    }
+
+    public override void SetPhoto()
+    {
+        this.Applicant.Photo = new Photo { Sort = "Photo 1" };
+    }
+
+    public override void SetUniversity()
+    {
+        this.Applicant.University = new University { Sort = "University of Pittsburgh, System Analisys, 2022" };
+    }
+    public override void SetExp()
+    {
+        this.Applicant.Exp = new Exp { Sort = "No previous experience" };
+    }
+
+}
+class Senior : AppBuilder
+{
+    public override void SetName()
+    {
+        this.Applicant.Name = new Name { Sort = "Hisoka Morow" };
+    }
+
+    public override void SetPhoto()
+    {
+    }
+
+    public override void SetUniversity()
+    {
+        this.Applicant.University = new University { Sort = "University of Pennsylvania, System Analisys, 1999;\nUniversity of Michigan, Informational Technologies, 2009" };
+    }
+    public override void SetExp()
+    {
+        this.Applicant.Exp = new Exp { Sort = "15 years of experience in CD Projekt Red, Bioware" };
+    }
+
+}
